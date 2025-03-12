1ebf7b6d30fb157c620711f6de98cb294bc17291
claraforhtml <clamap2@gmail.com>
2025-03-12 10:21:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function caricaContenuto(pagina) {
+function caricaContenuto(event, pagina) {
+    event.preventDefault();  // Impedisce il comportamento predefinito di navigazione
+
     // Crea una richiesta AJAX per caricare il contenuto HTML
     fetch(pagina)
         .then(response => response.text())  // Ottieni il contenuto della pagina come testo
