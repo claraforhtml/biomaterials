435a3d8df8254bbc247a3de1b9385f0935f83e4e
claraforhtml <clamap2@gmail.com>
2025-03-12 10:25:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ function caricaContenuto(event, pagina) {
         .then(response => response.text())  // Ottieni il contenuto della pagina come testo
         .then(data => {
             document.getElementById('contenuto').innerHTML = data;  // Inserisci il contenuto nella sezione "contenuto"
-            document.getElementById('backButton').style.display = 'inline';  // Mostra il bottone per tornare indietro
         })
         .catch(error => console.error('Errore nel caricamento del contenuto:', error));
-}
-
-// Funzione per tornare alla pagina principale
-function tornaAllaPaginaPrincipale() {
-    document.getElementById('contenuto').innerHTML = '';  // Rimuovi il contenuto caricato
-    document.getElementById('backButton').style.display = 'none';  // Nascondi il bottone di ritorno
 }
