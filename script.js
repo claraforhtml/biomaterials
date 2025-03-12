6506ec5b320ee2e8b253e4196ced8195b76f7e13
claraforhtml <clamap2@gmail.com>
2025-03-12 11:10:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function caricaContenuto(pagina) {
+    // Crea una richiesta AJAX per caricare il contenuto HTML
+    fetch(pagina)
+        .then(response => response.text())  // Ottieni il contenuto della pagina come testo
+        .then(data => {
+            document.getElementById('contenuto').innerHTML = data;  // Inserisci il contenuto nella sezione "contenuto"
+        })
+        .catch(error => console.error('Errore nel caricamento del contenuto:', error));
+}
 
